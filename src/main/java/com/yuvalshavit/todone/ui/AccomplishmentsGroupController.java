ad3d91aaae253fb251a7d52f07e8c581faddeb2a
Yuval Shavit <yankee.sierra@gmail.com>
2018-02-10 18:17:04 -0500

    rework how we add accomplishments
    Rather than adding them in bulk, let them get added one at a time. This
    means we can add a UI element to add them.
    
    Also make the tag chart a stacked area chart, and per-tag.

--- src/main/java/com/yuvalshavit/todone/ui/AccomplishmentsGroupController.java
+++ src/main/java/com/yuvalshavit/todone/ui/AccomplishmentsGroupController.java
@@ -1,25 +1,28 @@
 package com.yuvalshavit.todone.ui;
 
 import java.io.IOException;
-import java.util.Comparator;
+import java.util.function.ToLongFunction;
 
 import com.yuvalshavit.todone.data.Accomplishment;
 
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 
-public class AccomplishmentsGroupController {
+public class AccomplishmentsGroupController implements Comparable<AccomplishmentsGroupController> {
   public static final ControllerUtil.Renderer<AccomplishmentsGroupController> renderer = ControllerUtil.rendererFor(t -> t.top);
+
   @FXML protected Pane top;
   @FXML protected Label header;
   @FXML protected ListView<AccomplishmentController> accomplishments;
 
-  public static Comparator<AccomplishmentsGroupController> biggestFirst = (a, b) -> Integer.compare(b.accomplishmentsCount(), a.accomplishmentsCount());
+  private final ToLongFunction<AccomplishmentsGroupController> comparisonKey;
 
-  public AccomplishmentsGroupController() {
+  private AccomplishmentsGroupController(ToLongFunction<AccomplishmentsGroupController> comparisonKey) {
+    this.comparisonKey = comparisonKey;
     try {
       FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("accomplishment_group.fxml"));
       fxmlLoader.setController(this);
@@ -30,8 +33,14 @@ public class AccomplishmentsGroupController {
     }
   }
 
-  public void setHeader(String header) {
-    this.header.setText(header);
+  public static AccomplishmentsGroupController create(String headerPrefix, ToLongFunction<AccomplishmentsGroupController> comparisonKey) {
+    AccomplishmentsGroupController created = new AccomplishmentsGroupController(comparisonKey);
+    ListChangeListener<AccomplishmentController> changeListener = change -> {
+      int n = created.accomplishmentsCount();
+      created.header.setText(String.format("%s (%d accomplishment%s)", headerPrefix, n, n == 1 ? "" : "s"));
+    };
+    created.accomplishments.getItems().addListener(changeListener);
+    return created;
   }
 
   public void addAccomplishment(Accomplishment accomplishment) {
@@ -43,8 +52,13 @@ public class AccomplishmentsGroupController {
   }
 
   @Override
+  public int compareTo(AccomplishmentsGroupController o) {
+    // sort largest first
+    return - Long.compare(comparisonKey.applyAsLong(this), o.comparisonKey.applyAsLong(o));
+  }
+
+  @Override
   public String toString() {
-    int n = accomplishmentsCount();
-    return String.format("%s: %d accomplishment%s", header, n, n == 1 ? "" : "s");
+    return header.getText();
   }
 }
--- src/main/java/com/yuvalshavit/todone/ui/MainSceneController.java
+++ src/main/java/com/yuvalshavit/todone/ui/MainSceneController.java
@@ -6,124 +6,180 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NavigableMap;
 import java.util.ResourceBundle;
-import java.util.TreeMap;
+import java.util.function.ToLongFunction;
 
 import javax.inject.Inject;
 
+import com.google.common.collect.Lists;
 import com.yuvalshavit.todone.data.Accomplishment;
 import com.yuvalshavit.todone.data.Tagger;
 import com.yuvalshavit.todone.data.TodoneDao;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
 import javafx.util.StringConverter;
 
 public class MainSceneController implements Initializable {
   private static final int TAG_CHART_BUFFER = 1;
+  private static final StringConverter<Number> EPOCH_DAY_TICK_FORMATTER = new StringConverter<Number>() {
+    final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
+
+    @Override
+    public String toString(Number object) {
+      return formatter.format(LocalDate.ofEpochDay(object.longValue()));
+    }
+
+    @Override
+    public Number fromString(String string) {
+      return LocalDate.from(formatter.parse(string)).toEpochDay();
+    }
+  };
   private final LocalDate today = LocalDate.now();
 
   @FXML private ListView<AccomplishmentsGroupController> byDayList;
   @FXML private ListView<AccomplishmentsGroupController> byTagList;
-  @FXML protected LineChart<Long,Integer> tagsChart;
-  @FXML protected NumberAxis tagsChartX;
-  @FXML protected NumberAxis tagsChartY;
+  @FXML private XYChart<Long,Integer> tagsChart;
+  @FXML private NumberAxis tagsChartX;
+  @FXML private NumberAxis tagsChartY;
   @Inject protected ZoneId zoneId;
   @Inject protected TodoneDao dao;
 
+  private Map<LocalDate,AccomplishmentsGroupController> groupsByDay;
+  private Map<String,AccomplishmentsGroupController> groupsByTag;
+  /**
+   * Key is a tag. Series is epoch-day to count.
+   */
+  private Map<String,XYChart.Series<Long,Integer>> chartByTag;
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    NavigableMap<LocalDate,AccomplishmentsGroupController> byDayGroups = new TreeMap<>(Comparator.reverseOrder());
-    Map<String,AccomplishmentsGroupController> byTagGroups = new HashMap<>();
-    for (Accomplishment accomplishment : dao.fetchAll()) {
-      Instant instant = Instant.ofEpochMilli(accomplishment.getTimestamp());
-      LocalDate accomplishmentDate = instant.atZone(zoneId).toLocalDate();
-      byDayGroups.computeIfAbsent(accomplishmentDate, this::createGroupForDay).addAccomplishment(accomplishment);
-      for (String tag : Tagger.tagsFor(accomplishment.getText())) {
-        byTagGroups.computeIfAbsent(tag, this::createGroupForTag).addAccomplishment(accomplishment);
-      }
-    }
-    byDayList.getItems().addAll(byDayGroups.values());
-    XYChart.Series<Long,Integer> tagsChartSeries = new XYChart.Series<>();// TODO one series per tag
-    byDayGroups.descendingMap().forEach((date, group) -> {
-      XYChart.Data<Long, Integer> dataPoint = new XYChart.Data<>(date.toEpochDay(), group.accomplishmentsCount());
-      Label hoverLabel = new Label(String.valueOf(group.accomplishmentsCount()));
-
-      StackPane hoverPane = new StackPane();
-      hoverPane.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> {
-        hoverPane.getChildren().add(hoverLabel);
-      });
-      hoverPane.addEventHandler(MouseEvent.MOUSE_EXITED, e -> {
-        hoverPane.getChildren().clear();
-      });
-      dataPoint.setNode(hoverPane);
-      tagsChartSeries.getData().add(dataPoint);
-    });
-    tagsChartX.setTickLabelFormatter(new StringConverter<Number>() {
-      final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE;
-      @Override
-      public String toString(Number object) {
-        return formatter.format(LocalDate.ofEpochDay(object.longValue()));
-      }
-
-      @Override
-      public Number fromString(String string) {
-        return LocalDate.from(formatter.parse(string)).toEpochDay();
-      }
-    });
-    tagsChartX.setAutoRanging(false);
-    tagsChartX.setLowerBound(byDayGroups.lastKey().toEpochDay() - TAG_CHART_BUFFER);
-    tagsChartX.setUpperBound(LocalDate.now().toEpochDay() + TAG_CHART_BUFFER);
-    tagsChart.getData().add(tagsChartSeries);
-    byDayList.getItems().addAll(byDayGroups.values());
-    List<AccomplishmentsGroupController> tagGroups = new ArrayList<>(byTagGroups.values());
-    tagGroups.sort(AccomplishmentsGroupController.biggestFirst);
-    for (AccomplishmentsGroupController group : tagGroups) {
-      String header = group.header.getText();
-      int n = group.accomplishmentsCount();
-      header = String.format("%s (%d accomplishment%s)", header, n, n == 1 ? "" : "s");
-      group.header.setText(header);
-    }
-    byTagList.getItems().addAll(tagGroups);
+    groupsByDay = new HashMap<>();
+    groupsByTag = new HashMap<>();
+    chartByTag = new HashMap<>();
     byTagList.setCellFactory(AccomplishmentsGroupController.renderer);
     byDayList.setCellFactory(AccomplishmentsGroupController.renderer);
+
+    long todayEpoch = today.toEpochDay();
+    tagsChartX.setAutoRanging(false);
+    tagsChartX.setLowerBound(todayEpoch - TAG_CHART_BUFFER);
+    tagsChartX.setUpperBound(todayEpoch + TAG_CHART_BUFFER);
+    tagsChartX.setTickLabelFormatter(EPOCH_DAY_TICK_FORMATTER);
+    tagsChartY.setAutoRanging(false);
+    tagsChartY.setLowerBound(0);
+    tagsChartY.setUpperBound(1);
+
+    dao.fetchAll().forEach(this::addAccomplishment);
+  }
+
+  private void setChartRange(long epochDay) {
+    double newRangeLower = epochDay - TAG_CHART_BUFFER;
+    double newRangeUpper = epochDay + TAG_CHART_BUFFER;
+
+    if (tagsChartX.getLowerBound() < newRangeLower) {
+      tagsChartX.setLowerBound(newRangeLower);
+    }
+    if (tagsChartX.getUpperBound() > newRangeUpper) {
+      tagsChartX.setUpperBound(newRangeUpper);
+    }
+  }
+
+  public void addAccomplishment(Accomplishment accomplishment) {
+    LocalDate accomplishmentDate = Instant.ofEpochMilli(accomplishment.getTimestamp()).atZone(zoneId).toLocalDate();
+    long accomplishmentEpochDay = accomplishmentDate.toEpochDay();
+    // Add to the by-day list
+    AccomplishmentsGroupController groupForDay = groupsByDay.get(accomplishmentDate);
+    if (groupForDay == null) {
+      groupForDay = createGroupForDay(accomplishmentDate);
+      groupsByDay.put(accomplishmentDate, groupForDay);
+      byDayList.getItems().add(groupForDay);
+    }
+    groupForDay.addAccomplishment(accomplishment);
+
+    // Add to the by-tag lists
+    for (String tag : Tagger.tagsFor(accomplishment.getText())) {
+      AccomplishmentsGroupController groupForTag = groupsByTag.get(tag);
+      if (groupForTag == null) {
+        groupForTag = createGroupForTag(tag);
+        groupsByTag.put(tag, groupForTag);
+        byTagList.getItems().add(groupForTag);
+      }
+      groupForTag.addAccomplishment(accomplishment);
+      // add a blip to the chart
+      final XYChart.Data<Long, Integer> dataPoint;
+      XYChart.Series<Long, Integer> series = chartByTag.get(tag);
+      if (series == null) {
+        series = new XYChart.Series<>();
+        series.setName(tag);
+        chartByTag.put(tag, series);
+        tagsChart.getData().add(series);
+      }
+      ObservableList<XYChart.Data<Long, Integer>> seriesData = series.getData();
+      List<Long> seriesDataEpochDay = Lists.transform(seriesData, XYChart.Data::getXValue);
+      int searchResult = Collections.binarySearch(seriesDataEpochDay, accomplishmentEpochDay);
+      if (searchResult >= 0) {
+        dataPoint = seriesData.get(searchResult);
+      } else {
+        // See JavaDoc for binarySearch
+        // searchResult         = -(insertion point) - 1
+        // searchResult + 1     = -(insertion point)
+        // -(searchResult + 1)  = (insertion point)
+        int insertionPoint = -(searchResult + 1);
+        dataPoint = createTagPoint(accomplishmentEpochDay);
+        seriesData.add(insertionPoint, dataPoint);
+      }
+      int newY = 1 + dataPoint.getYValue();
+      dataPoint.setYValue(newY);
+      if (newY > tagsChartY.getUpperBound()) {
+        tagsChartY.setUpperBound(newY);
+      }
+    }
+
+    // set the chart range and sort the lists
+    setChartRange(accomplishmentEpochDay);
+    Collections.sort(byDayList.getItems());
+    Collections.sort(byTagList.getItems());
+  }
+
+  private XYChart.Data<Long, Integer> createTagPoint(long epochDay) {
+    // Set up the hover labels when you mouseover a point
+    // Label hoverLabel = new Label("");
+    // StackPane hoverPane = new StackPane();
+    // hoverPane.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> hoverPane.getChildren().add(hoverLabel));
+    // hoverPane.addEventHandler(MouseEvent.MOUSE_EXITED, e -> hoverPane.getChildren().clear());
+    // dataPoint.YValueProperty().addListener(((observable, oldValue, newValue) -> hoverLabel.setText(String.valueOf(newValue))));
+    // dataPoint.setNode(hoverPane);
+    return new XYChart.Data<>(epochDay, 0);
   }
 
   private AccomplishmentsGroupController createGroupForDay(LocalDate date) {
     final String header;
+    long epochDay = date.toEpochDay();
     if (date.equals(today)) {
       header = "Today";
     } else if (date.equals(today.minusDays(1))) {
       header = "Yesterday";
     } else {
-      long daysAgo = today.toEpochDay() - date.toEpochDay();
+      long daysAgo = today.toEpochDay() - epochDay;
       header = String.format("%s (%d days ago)", DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).format(date), daysAgo);
     }
-    return createAccomplishmentGroup(header);
+    return createAccomplishmentGroup(header, ignored -> epochDay);
   }
 
-  private AccomplishmentsGroupController createAccomplishmentGroup(String header) {
-    AccomplishmentsGroupController group = new AccomplishmentsGroupController();
-    group.setHeader(header);
-    return group;
+  private AccomplishmentsGroupController createAccomplishmentGroup(String header, ToLongFunction<AccomplishmentsGroupController> comparisonKey) {
+    return AccomplishmentsGroupController.create(header, comparisonKey);
   }
 
   private AccomplishmentsGroupController createGroupForTag(String tag) {
-    String header = "#" + tag;
-    return createAccomplishmentGroup(header);
+    // comparator is negated so that we sort largest-first
+    return createAccomplishmentGroup("#" + tag, AccomplishmentsGroupController::accomplishmentsCount);
   }
-
 }
