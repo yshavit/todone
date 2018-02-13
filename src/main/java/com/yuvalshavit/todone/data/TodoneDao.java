a07547943702ec40d5553e44f24b7ee35d7bb505
Yuval Shavit <yankee.sierra@gmail.com>
2018-02-13 02:39:45 -0500

    add tags() method

--- src/main/java/com/yuvalshavit/todone/data/TodoneDao.java
+++ src/main/java/com/yuvalshavit/todone/data/TodoneDao.java
@@ -1,6 +1,15 @@
 package com.yuvalshavit.todone.data;
 
+import java.util.stream.StreamSupport;
+
 public interface TodoneDao {
   void add(Accomplishment accomplishment);
   Iterable<Accomplishment> fetchAll();
+
+  default Iterable<String> tags() {
+    Iterable<Accomplishment> iterable = fetchAll();
+    return () -> StreamSupport.stream(iterable.spliterator(), false)
+      .flatMap(a -> Tagger.tagsFor(a.getText()).stream())
+      .iterator();
+  }
 }
